61e9bb61043b78715553c749dce9410b2eb5a0b2
Duplexes <Eccat@shaw.ca>
2021-02-02 20:26:53 -0800

    First Commit

---
+++ src/main/java/com/forsakensurvival/forsakencore/ForsakenCore.java
@@ -0,0 +1,17 @@
+package com.forsakensurvival.forsakencore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ForsakenCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
